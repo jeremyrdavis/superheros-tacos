7af72d29362be7b0f153a20f2edab5205a23cfce
jeremyrdavis <jeremy.davis@redhat.com>
2021-04-10 09:42:12 -0400

    extracted taco and beer methods

--- src/main/java/io/arrogantprogrammer/mutiny/superherotacos/api/MenuApiResource.java
+++ src/main/java/io/arrogantprogrammer/mutiny/superherotacos/api/MenuApiResource.java
@@ -32,6 +32,19 @@ public class MenuApiResource {
     ReactiveBeerClient beerClient;
 
     @GET
+    @Path("/randomtacoandbeer")
+    public Uni<String> getRandomTacoAndBeer() {
+        return Uni.<String>combine().all().unis(randomTaco(), randomBeer()).asTuple().map(tuple ->{
+            return new StringBuilder()
+                    .append("Today's Taco and Beer is a ")
+                    .append(tuple.getItem1())
+                    .append(" and a cold ")
+                    .append(tuple.getItem2())
+                    .toString();
+        });
+    }
+
+    @GET
     @Path("/dailytacoandbeer")
     public Uni<String> getDailyTacoAndBeer() {
         return Uni
@@ -87,5 +100,59 @@ public class MenuApiResource {
                 });
     }
 
-    
+    private Uni<String> randomBeer() {
+        return Multi.createBy()
+                .repeating()
+                .uni(AtomicInteger::new, page ->
+                        //getListOfBeers(page.incrementAndGet())
+                        beerClient.getBeersPage(page.incrementAndGet())
+                                .onFailure().recoverWithUni((Uni.createFrom().item(Collections.emptyList())))
+                )
+                .until(List::isEmpty)
+                .onItem()
+                .disjoint()
+                .collect()
+                .asList()
+                .onItem()
+                .transform(beers -> {
+                    return ((Beer) beers.get(new Random().nextInt(beers.size()))).getName();
+                });
+
+    }
+
+    private Uni<String> randomTaco() {
+        return Uni
+                .<String>combine()
+                .all()
+                .unis(
+                        tacoClient.getBaseLayers().onItem().transform(fillings -> {
+                            return fillings.get(new Random().nextInt(fillings.size())).getName();
+                        }),
+                        tacoClient.getCondiments().onItem().transform(condiments -> {
+                            return condiments.get(new Random().nextInt(condiments.size())).getName();
+                        }),
+                        tacoClient.getMixins().onItem().transform(mixins -> {
+                            return mixins.get(new Random().nextInt(mixins.size())).getName();
+                        }),
+                        tacoClient.getSeasonings().onItem().transform(seasonings -> {
+                            return seasonings.get(new Random().nextInt(seasonings.size())).getName();
+                        }),
+                        tacoClient.getShells().onItem().transform(shells -> {
+                            return shells.get(new Random().nextInt(shells.size())).getName();
+                        }))
+                .asTuple()
+                .map(t -> {
+                    return new StringBuilder()
+                            .append("Today's Taco and Beer is a ")
+                            .append(t.getItem1())
+                            .append(" with ")
+                            .append(t.getItem2())
+                            .append(", ")
+                            .append(t.getItem3())
+                            .append(", ")
+                            .append(t.getItem4())
+                            .append(", wrapped in a ")
+                            .append(t.getItem5()).toString();
+                });
+    }
 }
